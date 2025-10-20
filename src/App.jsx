1aefa383232ced389e795d8e4213bfa5ba1ed7fd
isabellafaitht <isabellafaitht2@gmail.com>
2025-10-20 14:22:33 +0800

    Initial commit: Complete React tutorial

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Profile from './Profile.jsx';
+
+function App() {
+  return (
+    <div>
+      <h1>My First React Component</h1>
+      <Profile />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Profile.jsx
@@ -0,0 +1,10 @@
+function Profile() {
+  return (
+    <img
+      src="C:\Users\63960\Documents\GitHub\react-tutorial\src\download (9) (1).png"
+      alt="Katherine Johnson"
+    />
+  );
+}
+
+export default Profile;
